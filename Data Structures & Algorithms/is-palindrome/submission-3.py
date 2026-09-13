a27ff5bab529e257f8bb4b08f0bda1d4ddd08865
ShriniTechie <shrinivas.softdev@gmail.com>
2026-09-13 11:05:24 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # raw_string = ""
+        # for char in s:
+        #     if char.isalnum():
+        #         raw_string += char 
+        # Since, storing raw_string requires O(n) space complexity
+        raw_string = "".join(
+            [char.lower() for char in s if char.isalnum()]
+        )
+
+        left = 0
+        right = len(raw_string)-1
+        raw_string = raw_string.lower()
+
+        while left < right:
+            if raw_string[left] != raw_string[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True  
+        
